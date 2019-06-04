e6c6a61ca51bcfdef2c6cb9be164934747355ca4
Matt Reeve <matt@basicinvite.com>
2019-06-04 17:37:14 -0600

    Adding 6 panel product

--- src/Demo.ts
+++ src/Demo.ts
@@ -12,7 +12,13 @@ export class Demo2 {
         
         let productDefinition = {
             surfaces: [
-                'basicinvite/Llama_Mamma_Birth_Announcement/Llama_Mamma_Birth_Announcement'
+                'basicinvite/Confetti_Dots_Storybook-1/Confetti_Dots_Storybook-1',
+                'basicinvite/Confetti_Dots_Storybook-2/Confetti_Dots_Storybook-2',
+                'basicinvite/Confetti_Dots_Storybook-3/Confetti_Dots_Storybook-3',
+                'basicinvite/Confetti_Dots_Storybook-4/Confetti_Dots_Storybook-4',
+                'basicinvite/Confetti_Dots_Storybook-5/Confetti_Dots_Storybook-5',
+                'basicinvite/Confetti_Dots_Storybook-6/Confetti_Dots_Storybook-6',
+                
             ],
         };
         let editorConfig = {
@@ -39,21 +45,25 @@ export class Demo2 {
     init() {
         document.addEventListener("DOMContentLoaded", async () => {
             let editor = await this.loadIFrame();
-            let product = await editor.getProduct();
-            let model = await product.getProductModel();
-            document.getElementById('changeColor').addEventListener('click', async () =>{
-                let items = await product.getItems((item: any, args: any) => ~item.name.indexOf(args.name), {name: 'text-'});
-                console.log('items to change', items)
-                for (let item of items) {
-                    if (item.color) {
-                        item.color = ColorFactory.createColor('#ff0000');
-                    }
-                    if (item.fillColor) {
-                        item.fillColor = ColorFactory.createColor('#ff0000');
-                    }
-                    await product.setItem(item);
-                }
-            });
+            // let product = await editor.getProduct();
+            // let model = await product.getProductModel();
+            // document.getElementById('changeColor').addEventListener('click', async () =>{
+            //     let items = await product.getItems((item: any, args: any) => item.name, {name: 'text-'});
+            //     console.log('items to change', items)
+            //     for (let item of items) {
+            //         if (item.color) {
+            //             console.log('before color', item.color);
+            //             let c =  ColorFactory.createColor('#ff0000');
+            //             item.color = c;
+            //             console.log('after color', c);
+                        
+            //         }
+            //         if (item.fillColor) {
+            //             item.fillColor = ColorFactory.createColor('#ff0000');
+            //         }
+            //         await product.setItem(item);
+            //     }
+            // });
         });
     }
 }
